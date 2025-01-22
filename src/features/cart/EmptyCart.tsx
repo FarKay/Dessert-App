85fac9186c99536b1eb9056661f98d1e37b371a2
Farouk <daudafarouk35@gmail.com>
2025-01-22 21:06:00 +0100

    modified images

--- src/features/cart/EmptyCart.tsx
+++ src/features/cart/EmptyCart.tsx
@@ -5,7 +5,10 @@ function EmptyCart() {
         Your Cart <span>(0)</span>
       </h1>
       <div className="flex flex-col justify-center items-center gap-3">
-        <img src="assets/images/illustration-empty-cart.svg" alt="empty-cart" />
+        <img
+          src="/assets/images/illustration-empty-cart.svg"
+          alt="empty-cart"
+        />
         <p>Your added items will appear here</p>
       </div>
     </div>
